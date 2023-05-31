a8b4b8a87ef3fe1cd98dded48998f5453053744a
Kimura <Ryunosuke_Kimura@totec.co.jp>
2023-05-31 13:52:37 +0900

    photo/viewsを編集

--- photoproject/photo/views.py
+++ photoproject/photo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+# django.views.genericからTemplateViewをインポート
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    # トップページのビュー
+    # index.htmlをレンダリングする
+    template_name ='index.html'
